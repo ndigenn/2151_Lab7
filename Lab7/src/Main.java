c9e3c53ab8d1c17536ba149695c021cddbc153ed
Nicholas DiGennaro <113647565+ndigenn@users.noreply.github.com>
2024-03-27 18:42:50 -0400

    final changes

--- Lab7/src/Main.java
+++ Lab7/src/Main.java
@@ -1,3 +1,5 @@
+package cpsc2150.listDec;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
