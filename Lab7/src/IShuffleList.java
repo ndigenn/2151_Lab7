dda59a199b9cdcd4b8f2b2f5d483cfe90b593cf8
brholde <156234663+brholde@users.noreply.github.com>
2024-03-27 17:58:32 -0400

    Added to IShuffleList swap contract

--- Lab7/src/IShuffleList.java
+++ Lab7/src/IShuffleList.java
@@ -7,11 +7,11 @@ import java.util.Random;
 public interface IShuffleList extends List {
 
     /**
-     * Randomly picks two positions in the list and swaps them the designated amount of times.
+     * Randomly picks two positions in the list and swaps them. It repeats this task the designated amount of times.
      *
      * @param swaps the desired amount of times that two random positions will be swapped.
      *
-     * 
+     * @post self = [self with items randomized to a varying degree]
      */
     default void shuffle(int swaps) {
         Random rand = new Random();
